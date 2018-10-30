2abe31c903e3fa54089b32cc933c0df20b6280c0
Mandar Chitre <mandar@arl.nus.edu.sg>
2018-10-30 19:38:22 +0800

    made quiver.py more generic to handle list/pull/push (yet to implement push)

--- quiver/quiver.py
+++ quiver/quiver.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 #
-# Extract Quiver note as markdown
+# Work with Quiver notes as markdown documents
 #
 # Usage:
-#   quiver2md.py [notebook-regex] [title-regex]
+#   quiver.py list [<notebook-regex>] [<title-regex>]
+#   quiver.py pull [<notebook-regex>] [<title-regex>]
+#   quiver.py push <markdown-file>
 #
 # We search the Quiver database for the matching notebook and note title.
-# A hyphen may be used in place of the notebook-regex to match all notebooks.
-# If a single note is matched, it is exported in markdown format. If multiple
+# A hyphen may be used in place of the <notebook-regex> to match all notebooks.
+# If a single note is matched, it can be exported in markdown format. If multiple
 # matches are found, a list of matching notes is displayed.
 #
 # The markdown format uses a yaml header with meta information, and "---"
@@ -23,6 +25,35 @@ home         = str(pathlib.Path.home())
 quiverRoot   = home+'/Dropbox/apps/Quiver.qvlibrary'     # Quiver notebook path
 trash        = 'Trash.qvnotebook'                        # Quiver trash notebook to ignore
 resources    = 'resources'                               # name or resource folder
+
+# usage
+def usage():
+  print('''
+Usage:
+  quiver.py list [<notebook-regex>] [<title-regex>]
+  quiver.py pull [<notebook-regex>] [<title-regex>]
+  quiver.py push <markdown-file>
+''')
+  exit(1)
+
+# arguments
+nargs = len(sys.argv)
+if nargs < 2 or nargs > 4:
+  usage()
+verb = sys.argv[1]
+if verb not in ['list', 'pull', 'push']:
+  usage()
+if verb == 'push':
+  if nargs != 3:
+    usage()
+  filename = sys.argv[2]
+else:
+  nb_regex = '-'
+  note_regex = '-'
+  if nargs > 2:
+    nb_regex = sys.argv[2]
+  if nargs > 3:
+    note_regex = sys.argv[3]
 
 # get list of notes
 notebook = ''
@@ -47,26 +78,31 @@ for root, subdirs, files in os.walk(quiverRoot):
         'root': root
       })
 
+# push handling
+if verb == 'push':
+  print('Not implemented yet')
+  exit(1)
+
 # filter by notebook
-if len(sys.argv) > 1 and sys.argv[1] != '-':
-  regex = re.compile(r'%s'%sys.argv[1], re.IGNORECASE)
+if nb_regex != '-':
+  regex = re.compile(r'%s'%nb_regex, re.IGNORECASE)
   notes = [n for n in notes if re.search(regex, n['notebook'])]
 
 # filter by note title
-if len(sys.argv) > 2 and sys.argv[2] != '-':
-  regex = re.compile(r'%s'%sys.argv[2], re.IGNORECASE)
+if note_regex != '-':
+  regex = re.compile(r'%s'%note_regex, re.IGNORECASE)
   notes = [n for n in notes if re.search(regex, n['title'])]
 
-# no matching notes, stop!
+# show notes
 if len(notes) == 0:
   print('No matching notes')
-  exit(1)
-
-# too many matching notes, stop!
-if len(notes) > 1:
-  print('# Notes')
-  for note in notes:
-    print('-', note['notebook'], '::', note['title'])
+else:
+  if verb == 'list' or len(notes) > 1:
+    for note in notes:
+      print(note['notebook'], '::', note['title'])
+if verb == 'list':
+  exit(0)
+if len(notes) != 1:
   exit(2)
 
 # read the note
@@ -78,8 +114,8 @@ filename = os.path.join(note['root'], 'content.json')
 with open(filename, encoding='utf-8') as f:
   data = json.load(f)
 
-# export to file
-print('Output:', note['uuid']+'.md')
+# pull handling
+print('Created', note['uuid']+'.md')
 with open(note['uuid']+'.md', 'w') as f:
 
   # yaml block
@@ -122,10 +158,11 @@ with open(note['uuid']+'.md', 'w') as f:
 
 # copy resources out
 src_resources = os.path.join(note['root'], 'resources')
-try:
-  os.mkdir(resources)
-except:
-  pass
-for f in os.listdir(src_resources):
-  print('Resource:', f)
-  shutil.copyfile(os.path.join(src_resources, f), os.path.join(resources, f))
+if os.path.isdir(src_resources):
+  try:
+    os.mkdir(resources)
+  except:
+    pass
+  for f in os.listdir(src_resources):
+    print('Created', f)
+    shutil.copyfile(os.path.join(src_resources, f), os.path.join(resources, f))
